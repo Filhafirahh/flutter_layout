4a518cb5e35861b491255e481c5c9afc638faa91
unknown <filhafirah2000016071@webmail.uad.ac.id>
2024-04-29 22:37:48 +0700

    tambah stack

---
+++ stack/lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Stack'),
+        ),
+        body: Center(
+            child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.center,
+              child: Container(
+                color: Colors.red,
+                height: 100,
+                width: 100,
+              ),
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: Container(
+                color: Colors.blue,
+                height: 100,
+                width: 80,
+                margin: EdgeInsets.only(top: 20),
+              ),
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: Container(
+                color: Colors.purple,
+                height: 70,
+                width: 70,
+                margin: EdgeInsets.only(top: 20),
+              ),
+            )
+          ],
+        )),
+      ),
+    );
+  }
+}
